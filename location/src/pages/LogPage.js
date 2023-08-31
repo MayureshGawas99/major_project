60bf5d347138d330ee6d86b135e799ff8e884351
MayureshGawas99 <narayan.gawas@spit.ac.in>
2023-09-01 00:13:12 +0530

    added profile

--- location/src/pages/LogPage.js
+++ location/src/pages/LogPage.js
@@ -34,6 +34,31 @@ export default function LogPage() {
               </div>
               <div className="d-flex flex-row justify-content-start">
                 <img
+                  style={{ width: 45, height: "100%" }}
+                  src={`./images/${
+                    localStorage.getItem("image")
+                      ? localStorage.getItem("image")
+                      : 5
+                  }.jpg`}
+                  alt="avatar"
+                  className="rounded-circle img-fluid"
+                />
+                <div>
+                  <p
+                    className="small p-2 ms-3 mb-1 rounded-3"
+                    style={{ backgroundColor: "#f5f6f7" }}
+                  >
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua.
+                  </p>
+                  <p className="small ms-3 mb-3 rounded-3 text-muted float-end">
+                    12:00 PM | Aug 13
+                  </p>
+                </div>
+              </div>
+              <div className="d-flex flex-row justify-content-start">
+                <img
                   src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava6-bg.webp"
                   alt="avatar 1"
                   style={{ width: 45, height: "100%" }}
